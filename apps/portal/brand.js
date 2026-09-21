189ccd83ff9e2970556dd802e5da88513f354236
Justin Middler <justin@yumait.com.au>
2026-09-21 10:50:18 +1000

    Crop shared logo cleanly above wordmark

--- apps/portal/brand.js
+++ apps/portal/brand.js
@@ -7,6 +7,5 @@ const artwork = fs.readFileSync(path.join(__dirname, 'brand-home-logo.jpg')).toS
 function svg(viewBox, title) {
   return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="${viewBox}" role="img" aria-label="${title}"><title>${title}</title><image width="1200" height="630" xlink:href="data:image/jpeg;base64,${artwork}"/></svg>`;
 }
-const LOGO_SVG = svg('410 0 385 375', 'Blak Workspace');
-const WORDMARK_SVG = svg('0 0 1200 630', 'Blak Workspace by Yuma IT');
-module.exports = { LOGO_SVG, WORDMARK_SVG };
+const LOGO_SVG = svg('410 0 385 370', 'Blak Workspace');
+module.exports = { LOGO_SVG };
